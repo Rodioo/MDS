770d6491c8ffa8b4a220d3045671501f6027d7dc
G Bostinel <79469458+gogurazvan@users.noreply.github.com>
2022-05-15 02:41:13 +0300

    map template

---
+++ ProiectMDS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    public Rigidbody2D rb;
+
+    public Camera cam;
+
+    Vector2 movement;
+
+    Vector2 mousePos;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        
+        if(movement.x != 0 && movement.y != 0)
+        {
+            rb.MovePosition(rb.position + movement * (moveSpeed/2) * Time.fixedDeltaTime);
+        }
+        else{
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        Vector2 lookDir = mousePos -rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+}
