81f78c9e9bb4554dbd6b63d2bf2a56591312da64
Octi21 <ochiriac85@gmail.com>
2022-06-10 23:41:20 +0300

    spider ok

--- ProiectMDS/Assets/Scripts/Spider.cs
+++ ProiectMDS/Assets/Scripts/Spider.cs
@@ -6,7 +6,7 @@ public class Spider : MonoBehaviour
 {
     public Animator animator;
 
-    public Transform player;
+    private Transform player;
     private Rigidbody2D rb;
     private Vector2 movement;
     public float moveSpeed = 1f;
@@ -16,6 +16,8 @@ public class Spider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         rb = this.GetComponent<Rigidbody2D>();
     }
 
