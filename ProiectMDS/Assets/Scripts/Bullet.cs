8aa2af43856a9e95e555655f4c3b6939593342b2
Rodioo <antonioalex459@gmail.com>
2022-06-10 17:33:10 +0300

    added initial damage for player and spider

--- ProiectMDS/Assets/Scripts/Bullet.cs
+++ ProiectMDS/Assets/Scripts/Bullet.cs
@@ -1,19 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System;
+
 
 public class Bullet : MonoBehaviour
 {
     
     //Coliziune
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!collision.gameObject.CompareTag("Player"))
             Destroy(gameObject);
         if (collision.gameObject.name == "HitBox")
             MenuCollision.checkMenuHit(collision);
     }
+
 }
